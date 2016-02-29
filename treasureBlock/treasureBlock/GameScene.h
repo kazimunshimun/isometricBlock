c906586861dff4ad737de01f278b3b93abddd2ba
kazimunshimun <anik1.bd@gmail.com>
2016-02-29 13:27:03 +0600

    Initial project creation commit

---
+++ treasureBlock/treasureBlock/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  treasureBlock
+//
+
+//  Copyright (c) 2016 Anik. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ treasureBlock/treasureBlock/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  treasureBlock
+//
+
+//  Copyright (c) 2016 Anik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
